ffb4923b01309eb727c4171a16b4d5cc0c0e17d9
Miguel-Pariente <miguelpl32@hotmail.com>
2021-08-18 21:00:38 +0200

    Creacion de componentes compartidos, creacion de botones para cambiar de un tab a otro y boton de back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,30 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'inicio',
+    loadChildren: () =>
+      import('./pages/inicio/inicio.module').then((m) => m.InicioPageModule),
+  },
+  {
+    path: 'alert',
+    loadChildren: () =>
+      import('./pages/alert/alert.module').then((m) => m.AlertPageModule),
+  },
+  {
+    path: 'action-sheet',
+    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
