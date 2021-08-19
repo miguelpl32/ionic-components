4612f26c8109447297c70f05d42f718a1e225f8a
Miguel-Pariente <miguelpl32@hotmail.com>
2021-08-19 20:04:45 +0200

    Generacion de los distintos tipos de botones y configuracion de boton personalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,11 @@ const routes: Routes = [
   {
     path: 'avatar',
     loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)
+  },  {
+    path: 'button',
+    loadChildren: () => import('./pages/button/button.module').then( m => m.ButtonPageModule)
   },
+
 ];
 
 @NgModule({
