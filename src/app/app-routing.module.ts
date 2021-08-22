325a7fd68ee38d74d740a8592829bd610bc1a1a7
Miguel-Pariente <miguelpl32@hotmail.com>
2021-08-22 19:13:13 +0200

    Generar Toast sin opciones y con opciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -122,6 +122,10 @@ const routes: Routes = [
     path: 'tabs',
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
+  {
+    path: 'toast',
+    loadChildren: () => import('./pages/toast/toast.module').then( m => m.ToastPageModule)
+  },
 
 ];
 
