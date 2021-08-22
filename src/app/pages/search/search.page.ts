52ea9e56fff149b00d922204c11a7edc7f149ce4
Miguel-Pariente <miguelpl32@hotmail.com>
2021-08-22 11:49:30 +0200

    Generacion pagina para buscar, creacion de Pipe personalizado filtrar por titulo, servicio para obtener datos para filtrar

---
+++ src/app/pages/search/search.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.page.html',
+  styleUrls: ['./search.page.scss'],
+})
+export class SearchPage implements OnInit {
+  albumes: any[] = [];
+  textoBuscar = '';
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getAlbumes().subscribe((albumes) => {
+      // console.log(albumes);
+      this.albumes = albumes;
+    });
+  }
+  onSearchChange(event) {
+    // console.log(event);
+    this.textoBuscar = event.detail.value;
+  }
+}
