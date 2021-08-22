2018c8023c342c92daaf8c4591a7a393583ee0e9
Miguel-Pariente <miguelpl32@hotmail.com>
2021-08-22 10:47:18 +0200

    Generacion barra de progreso y controlar el valor de la barra mediante un evento

---
+++ src/app/pages/progress/progress.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress',
+  templateUrl: './progress.page.html',
+  styleUrls: ['./progress.page.scss'],
+})
+export class ProgressPage implements OnInit {
+  porcentaje = 0.5;
+  constructor() {}
+
+  ngOnInit() {}
+
+  rangeChange(event) {
+    //console.log(event.detail.value);
+    this.porcentaje = event.detail.value / 100;
+    console.log(this.porcentaje);
+  }
+}
